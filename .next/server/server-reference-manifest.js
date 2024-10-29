725e81ba10ddc8503107e68d10d873d7cacc6345
FerlinFeliz <ferlin1830@gmail.com>
2024-10-29 14:41:11 -0400

    .

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"E/1EF492Vj5voDYQDhc9zJOcfm4VWfIw755r7PNakPA=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"YN/KtiDyF7kjGlk2XXbKhTnTJkCZHBdpv/dWyCXGvsI=\"\n}"
->>>>>>> 0033b5b69 (first commit)
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"MujmhjjgkX/KVbgd6X5SsaMzORJZOT2F9nuGs7KoNXY=\"\n}"
